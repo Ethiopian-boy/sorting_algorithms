59f2af910202df1ad17b6e5ec5fff26fb2a1e768
Ethiopianboy <mullerfromethiopia@gmail.com>
2022-07-19 11:42:35 -0700

    Sorting algorithms & Big O

---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Selection sort function
+ *  @size: size array
+ *  @array: pointer to the array to be sorted
+ *  Return: Nothing
+ */
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i, j, min, swap;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size - 1; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+
+		}
+		if (min != i)
+		{
+			swap = array[min];
+			array[min] = array[i];
+			array[i] = swap;
+			print_array(array, size);
+		}
+	}
+}
